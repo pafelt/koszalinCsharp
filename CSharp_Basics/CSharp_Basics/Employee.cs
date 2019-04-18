66a63d1a8c0e0e66a685dfe1dfbf9e4668d5e20c
paaf1539 <extern.patryk.afelt@elektrobit.com>
2019-04-18 09:31:27 +0200

    Merge done x2

--- CSharp_Basics/CSharp_Basics/Employee.cs
+++ CSharp_Basics/CSharp_Basics/Employee.cs
@@ -1,52 +1,52 @@
-﻿using System;
+﻿using System/;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace CSharp_Basics
 {
     public class Employee
     {
         public string Name { get; }
-        public int BadgeID { get; }
-        public Locations Location { get; }
-
-        public Employee(string name, int badgeId, string location)
+        public int BadgeID { get;  }
+        public Locations Location {get; }
+        public Employee(string name, int badgeID, string location)
         {
+            
 
             this.Name = name;
             this.BadgeID = badgeID;
             Locations temp;
             Enum.TryParse(location, out temp);
             Location = temp;
-            ValidateBadgeNumber();
+            ValidateBadgeNumber(BadgeID, Location);
         }
         public Employee(string name, int badgeID, Locations location)
         {
+            ValidateBadgeNumber(BadgeID, location);
             this.Name = name;
             this.BadgeID = badgeID;
             this.Location = location;
-            ValidateBadgeNumber();
+           
         }
 
-        private void ValidateBadgeNumber()
+        private void ValidateBadgeNumber(int badgeID, Locations location)
         {
-            if(BadgeID < 10000 || BadgeID > 99999)
+           if (badgeID < 10000 && badgeID > 99999)
             {
                 throw new ArgumentOutOfRangeException("Badge should have 5 digits");
             }
-
-            if (IsNorthOfWarsaw() && BadgeID >= 60000)
+           if(IsNorthWarsaw() && BadgeID >= 60000)
             {
                 return;
             }
-
-            if (!IsNorthOfWarsaw() && BadgeID < 60000)
+            if (!IsNorthWarsaw() && BadgeID < 60000)
             {
                 return;
             }
-            throw new ArgumentOutOfRangeException("Bagde is from wrong region");
+            throw new ArgumentOutOfRangeException("Badge is from wrong region");
         }
 
         public string LocationToString()
@@ -56,6 +56,9 @@ namespace CSharp_Basics
         
         public bool IsNorthWarsaw()
         {
+            // polnoc id - 1-5 - 5 cyfr
+            // poludnie id - 6-9 - - 5 cyfr
+            // throw new ArgumentOutOfRangeException();
             if (Location == Locations.Koszalin || Location == Locations.Szczecin)
             {
                 return true;
@@ -66,5 +69,6 @@ namespace CSharp_Basics
             }
 
         }
+   
     }
 }
